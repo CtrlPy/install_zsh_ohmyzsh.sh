adb14ab4d2403629aafac955842328ba29055544
ctrlpy <ctrlpy3@gmail.com>
2024-08-19 22:04:45 +0300

    feat: new theme and plugin install

--- install_zsh_ohmyzsh.sh
+++ install_zsh_ohmyzsh.sh
@@ -3,8 +3,23 @@
 # Install Zsh
 sudo apt update && sudo apt install -y zsh
 
-# Change default shell to Zsh
+# Change the default shell to Zsh
 chsh -s $(which zsh)
 
 # Install Oh-My-Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install Powerlevel10k theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
+
+# Install essential plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-completions
+
+# Add plugins to .zshrc
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting zsh-completions)/g' ~/.zshrc
+
+# Restart Zsh to apply changes
+exec zsh
