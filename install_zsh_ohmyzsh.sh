87979a8689899e1bbabf3bc5439b93f2247da090
ctrlpy <ctrlpy3@gmail.com>
2024-08-20 11:47:42 +0300

    fix: fff 5

--- install_zsh_ohmyzsh.sh
+++ install_zsh_ohmyzsh.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# Ensure the script is not run as root
+if [ "$EUID" -eq 0 ]; then
+  echo "Please do not run as root"
+  exit
+fi
+
 # Install Zsh without user interaction
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y zsh
 
-# Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Install Oh My Zsh for the current user
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
 
 # Install Powerlevel10k theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
@@ -14,28 +20,14 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # Set Powerlevel10k as the default theme and enable plugins in .zshrc
-if [ ! -f ~/.zshrc ]; then
-    cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
-    sed -i "s/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/g" ~/.zshrc
-else
-    sed -i "s/ZSH_THEME=\".*\"/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/g" ~/.zshrc
-fi
-
-# Add plugins to .zshrc
+sed -i "s/ZSH_THEME=\".*\"/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/g" ~/.zshrc
 sed -i "s/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/" ~/.zshrc
 
 # Source the .zshrc to apply the changes
-echo 'source ~/.zshrc' >> ~/.zshrc
+source ~/.zshrc
 
-# Change default shell to Zsh
-chsh -s $(which zsh) $USER
-
-# Ensure Zsh is set as the default shell
-echo "SHELL is set to $SHELL"
-if [ "$SHELL" != "$(which zsh)" ]; then
-    echo "Changing default shell to zsh"
-    chsh -s $(which zsh)
-fi
+# Change default shell to Zsh for the current user
+chsh -s $(which zsh)
 
 # Prompt user to restart the terminal
-echo "Installation complete. Please restart your terminal."
+echo "Installation complete. Please restart your terminal or run 'exec zsh'."
