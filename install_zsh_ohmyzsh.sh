d887be8bf622190f44f6a606caf504716fa74356
ctrlpy <ctrlpy3@gmail.com>
2024-08-21 00:54:34 +0300

    fix: fff000

--- install_zsh_ohmyzsh.sh
+++ install_zsh_ohmyzsh.sh
@@ -1,48 +1,74 @@
 #!/bin/bash
 
+# Ensure the script is not run as root
+if [ "$EUID" -eq 0 ]; then
+  echo "Please do not run as root"
+  exit
+fi
+
+# Function to install Zsh based on the detected OS
+install_zsh() {
+    if [ -f /etc/debian_version ]; then
+        # Debian/Ubuntu
+        sudo DEBIAN_FRONTEND=noninteractive apt-get install -y zsh
+    elif [ -f /etc/arch-release ]; then
+        # Arch Linux
+        sudo pacman -Syu --noconfirm zsh
+    elif [ -f /etc/redhat-release ]; then
+        # RedHat/CentOS/Fedora
+        sudo yum install -y zsh
+    elif [ "$(uname)" == "Darwin" ]; then
+        # macOS
+        brew install zsh
+    else
+        echo "Unsupported OS. Please install Zsh manually."
+        exit 1
+    fi
+}
+
 # Install Zsh
-sudo apt-get update && sudo apt-get install -y zsh
+install_zsh
 
-# Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
+# Install Oh My Zsh for the current user
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
 
 # Wait for Oh My Zsh installation to complete
 sleep 5
 
 # Install Powerlevel10k theme
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
 
 # Install plugins: zsh-autosuggestions and zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-# Set Powerlevel10k as the default theme and enable plugins in .zshrc
-if [ ! -f ~/.zshrc ]; then
-    cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
-    sed -i "s/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/g" ~/.zshrc
-else
-    sed -i "s/ZSH_THEME=\".*\"/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/g" ~/.zshrc
+if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
 
-# Add plugins to .zshrc
-sed -i "s/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/" ~/.zshrc
+if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+
+# Ensure the .zshrc file is ready before making changes
+if [ -f ~/.zshrc ]; then
+    # Enable plugins in .zshrc
+    sed -i "s/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/" ~/.zshrc
+else
+    echo ".zshrc file not found. Please check the installation process."
+    exit 1
+fi
 
 # Source the .zshrc to apply the changes
-echo 'source ~/.zshrc' >> ~/.zshrc
+source ~/.zshrc
 
-# Change default shell to Zsh
-chsh -s $(which zsh) $USER
-
-# Ensure Zsh is set as the default shell
-echo "SHELL is set to $SHELL"
-if [ "$SHELL" != "$(which zsh)" ]; then
-    echo "Changing default shell to zsh"
-    chsh -s $(which zsh)
+# Attempt to change default shell to Zsh for the current user
+echo "Attempting to change default shell to Zsh..."
+if chsh -s $(which zsh); then
+    echo "Shell changed successfully!"
+else
+    echo "Failed to change shell. Please change your shell manually using 'chsh -s $(which zsh)' and entering your password."
 fi
 
-# Prompt user to restart the terminal
-echo "Installation complete. Please restart your terminal."
-
-# Automatically switch to Zsh
-exec zsh
+# Prompt the user to start a new Zsh session
+echo "Installation complete. Starting Powerlevel10k configuration..."
+exec zsh -c 'p10k configure'
